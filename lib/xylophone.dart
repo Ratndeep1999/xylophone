554361542620c5701a589c862650bf7cda5ba1b8
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-16 15:17:44 +0530

    testing button press output

--- lib/xylophone.dart
+++ lib/xylophone.dart
@@ -14,32 +14,70 @@ class Xylophone extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              XyloPhoneButtons(buttonColor: Colors.red,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.orange,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.yellow,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.green,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.lightBlue.shade400,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.blue.shade700),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.purple,),
-              SizedBox(height: 5,),
-              XyloPhoneButtons(buttonColor: Colors.pink,),
-
+              XyloPhoneButtons(
+                buttonColor: Colors.red,
+                playSound: () {
+                  debugPrint('C');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.orange,
+                playSound: () {
+                  debugPrint('B');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.yellow,
+                playSound: () {
+                  debugPrint('D');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.green,
+                playSound: () {
+                  debugPrint('A');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.lightBlue.shade400,
+                playSound: () {
+                  debugPrint('E');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.blue.shade700,
+                playSound: () {
+                  debugPrint('F');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.purple,
+                playSound: () {
+                  debugPrint('G');
+                },
+              ),
+              SizedBox(height: 5),
+              XyloPhoneButtons(
+                buttonColor: Colors.pink,
+                playSound: () {
+                  debugPrint('H');
+                },
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
   Future<void> playSound() async {
     final player = AudioPlayer();
     await player.play(UrlSource('https://example.com/my-audio.wav'));
   }
 }
-
-
