aee2e54f7c8c6d60824fcf4376d3c887020b46b3
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-16 10:24:46 +0530

    xylophone.dart :

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xylophone/xylophone.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      // home: xyloPhoneButton(),
+      home: Xylophone(),
     );
   }
 }
---
+++ lib/xylophone.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
